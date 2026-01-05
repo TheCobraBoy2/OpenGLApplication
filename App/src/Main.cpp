e1c54f1423fe99460e5dd20f83a2a65262dbd9d8
Landon Snellgrove <154959204+TheCobraBoy2@users.noreply.github.com>
2026-01-04 20:09:46 -0600

    Initial Commit

---
+++ App/src/Main.cpp
@@ -0,0 +1,17 @@
+#include "Core/Application.h"
+
+#include "AppLayer.h"
+#include "OverlayLayer.h"
+
+int main()
+{
+  Core::ApplicationSpecification appSpec;
+  appSpec.Name = "Architecture";
+  appSpec.WindowSpec.Width = 1920;
+  appSpec.WindowSpec.Height = 1080;
+
+  Core::Application application(appSpec);
+  application.PushLayer<AppLayer>();
+  application.PushLayer<OverlayLayer>();
+  application.Run();
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# OpenGL Application
+
+---
+This application used OpenGL, glad, glfw, and glm to create a small application
